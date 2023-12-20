dd4dcd30062776c6fb48cf16e591da2cc392f2c0
nikhilkhetan17 <nikhilkh1198@gmail.com>
2023-12-20 21:24:41 +0530

    created page not found component

--- todo-tracker-frontend/src/app/app.module.ts
+++ todo-tracker-frontend/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import {MatButtonModule} from '@angular/material/button';
     HomeComponent,
     LoginComponent,
     HeaderComponent,
-    SignUpComponent
+    SignUpComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
